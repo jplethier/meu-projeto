c4ae075805e93ac678c937346dbb007148e1fe04
jplethier <jplethier@gmail.com>
2011-02-10 21:08:25 -0200

    tela para perfil do usuário criada

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+        @title = @user.name
+    end
     
     def novocadastro
         @user = User.new
